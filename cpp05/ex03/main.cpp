0554a6356c361ea38948b20cfc2f34f25e3698b5
Seongil Yoon <seonyoon@c4r8s8.42seoul.kr>
2024-04-22 16:27:34 +0900

    add more test main.c

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seonyoon <seonyoon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 21:59:57 by seonyoon          #+#    #+#             */
-/*   Updated: 2024/04/07 22:40:21 by seonyoon         ###   ########.fr       */
+/*   Updated: 2024/04/22 16:25:22 by seonyoon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,22 @@
 #include "ShrubberyCreationForm.hpp"
 
 int main(void) {
+    AForm *f1;
     Intern intern;
     Bureaucrat b1("b1", 1);
     std::cout << b1 << std::endl;
-    AForm *f1 = intern.makeForm("presidential pardon", "f1");
-    std::cout << *f1 << std::endl;
+    try {
+        f1 = intern.makeForm("wrong form", "f1");
+        std::cout << *f1 << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    try {
+        f1 = intern.makeForm("presidential pardon", "f1");
+        std::cout << *f1 << std::endl;
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
     b1.signForm(*f1);
     std::cout << *f1 << std::endl;
     b1.executeForm(*f1);
