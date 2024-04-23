a73b1e09355a8d5dc38e1f5699b589a7948fa50e
Seongil Yoon <seonyoon@c4r8s8.42seoul.kr>
2024-04-23 19:24:52 +0900

    cpp09

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -6,6 +6,25 @@
 /*   By: seonyoon <seonyoon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 23:03:55 by seonyoon          #+#    #+#             */
-/*   Updated: 2024/04/19 23:03:55 by seonyoon         ###   ########.fr       */
+/*   Updated: 2024/04/23 19:24:44 by seonyoon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+#include <iostream>
+
+BitcoinExchange::BitcoinExchange(void) {}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &ref) {
+    this->db = ref.db;
+}
+
+BitcoinExchange::~BitcoinExchange(void) {}
+
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &ref) {
+    if (this == &ref)
+        return *this;
+    this->db = ref.db;
+    return *this;
+}
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,6 +6,43 @@
 /*   By: seonyoon <seonyoon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 23:03:52 by seonyoon          #+#    #+#             */
-/*   Updated: 2024/04/19 23:03:52 by seonyoon         ###   ########.fr       */
+/*   Updated: 2024/04/23 18:53:46 by seonyoon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef BITCOIN_EXCHANGE_HPP
+#define BITCOIN_EXCHANGE_HPP
+
+#include <map>
+#include <string>
+
+class BitcoinExchange {
+  private:
+    std::map<std::string, double> db;
+
+  public:
+    BitcoinExchange(void);
+    BitcoinExchange(const BitcoinExchange &ref);
+    ~BitcoinExchange(void);
+    BitcoinExchange &operator=(const BitcoinExchange &ref);
+
+    void readFile(std::string &filename);
+    void printResult(void);
+
+    class NegativeNumberException : public std::exception {
+      public:
+        virtual const char *what() const throw();
+    };
+
+    class LargeNumberException : public std::exception {
+      public:
+        virtual const char *what() const throw();
+    };
+
+    class BadInputException : public std::exception {
+      public:
+        virtual const char *what() const throw();
+    };
+};
+
+#endif
