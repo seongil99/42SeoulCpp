a09aa40ec4657fd81381a17abbb108d383861ebb
Seongil Yoon <seonyoon@c4r8s8.42seoul.kr>
2024-05-24 19:23:40 +0900

    cpp 09

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: seonyoon <seonyoon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 23:03:55 by seonyoon          #+#    #+#             */
-/*   Updated: 2024/05/04 18:36:17 by seonyoon         ###   ########.fr       */
+/*   Updated: 2024/05/24 16:55:48 by seonyoon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,30 @@ BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &ref) {
 }
 
 bool BitcoinExchange::_checkDate(const std::string &date) const {
-    for (size_t i = 0; i < date.size(); i++) {
+    std::string temp = date;
+    int year = 0, month = 0, day = 0, cnt = 0;
+    for (size_t i = 0; i < temp.size(); i++) {
+        if (temp[i] != '-' && !std::isdigit(temp[i]))
+            return false;
+        if (temp[i] == '-') {
+            temp[i] = ' ';
+            cnt++;
+        }
     }
+    if (cnt != 2)
+        return false;
+    std::istringstream ss(temp);
+    ss >> year;
+    ss >> month;
+    ss >> day;
+    std::cout << "check date:" << year << ' ' << month << ' ' << day
+              << std::endl;
+    if (year < 0)
+        return false;
+    if (month < 1 || month > 12)
+        return false;
+    if (day < 1 || day > 31)
+        return false;
     return true;
 }
 
@@ -48,14 +70,24 @@ double BitcoinExchange::_convertValue(const std::string &value) const {
         throw BitcoinExchange::NegativeNumberException();
     if (ret > 1000)
         throw BitcoinExchange::LargeNumberException();
-    if (*end || static_cast<int>(ret) != ret)
+    if (*end)
         throw BitcoinExchange::BadInputException();
     return ret;
 }
 
-bool cmp(std::map<std::string, double>::iterator &s1,
-         std::map<std::string, double>::iterator &s2) {
-    return (*s1).first < (*s2).first;
+bool cmp(const std::pair<const std::string, double> &s1,
+         const std::pair<const std::string, double> &s2) {
+    return (s1).first > (s2).first;
+}
+
+bool cmp1(const std::pair<const std::string, double> &pair,
+          const std::string &value) {
+    return pair.first < value;
+}
+
+bool cmp2(const std::string &value,
+          const std::pair<const std::string, double> &pair) {
+    return value < pair.first;
 }
 
 void BitcoinExchange::readInputFile(const char *filename) {
@@ -65,15 +97,20 @@ void BitcoinExchange::readInputFile(const char *filename) {
 
     std::string line;
     while (std::getline(file, line)) {
-        size_t idx = line.find('|');
-        std::string date = line.substr(0, idx - 1);
-        std::string value = line.substr(idx + 1);
-        std::map<std::string, double>::iterator f = db.find(date);
-        double t = _convertValue(value);
-        std::map<std::string, double>::iterator tt =
-            std::lower_bound(db.begin(), db.end(), date, cmp);
-        if (f != db.end())
-            std::cout << date << " => " << (*tt).second * t << std::endl;
+        try {
+            size_t idx = line.find('|');
+            std::string date = line.substr(0, idx - 1);
+            std::string value = line.substr(idx + 1);
+            _checkDate(date);
+            std::map<std::string, double>::iterator f = db.find(date);
+            double t = _convertValue(value);
+            std::map<std::string, double>::iterator tt =
+                std::lower_bound(db.begin(), db.end(), date, cmp1);
+            if (f != db.end())
+                std::cout << date << " => " << (*tt).second * t << std::endl;
+        } catch (const std::exception &e) {
+            std::cerr << e.what() << std::endl;
+        }
     }
 }
 
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seonyoon <seonyoon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 23:03:58 by seonyoon          #+#    #+#             */
-/*   Updated: 2024/04/26 15:06:03 by seonyoon         ###   ########.fr       */
+/*   Updated: 2024/05/24 16:56:25 by seonyoon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,10 @@
 
 int main(int argc, char **argv) {
     BitcoinExchange exchange;
-
     if (argc != 2) {
         std::cerr << "Usage: ./btc [filename]" << std::endl;
         return 1;
     }
-    try {
-        exchange.run(argv[1]);
-    } catch (std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
+    exchange.run(argv[1]);
     return 0;
 }
